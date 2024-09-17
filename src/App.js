560b2ab41113ee5e6a2e09b0a2bf830613182317
Lostsoul12345 <bad.joke0011@gmail.com>
2024-09-17 14:23:43 +0330

    Episode 2 - Project Creation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+function LoginPage(){
+  return(
+    <div>
+      <h1>Login Page</h1>
+      <div style={{fontSize: "1.5rem"}}>
+        This is our component
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
