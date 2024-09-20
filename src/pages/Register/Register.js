10b545aad6d91bd43c8cf9b1051327d50bd4436e
Lostsoul12345 <bad.joke0011@gmail.com>
2024-09-21 01:06:30 +0330

    Episode 4 - useState

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,8 +1,40 @@
+import { useState } from "react"
 
-function Register(){
-    return(
+function Register() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onInputChange = (event) => {
+        console.log(event);
+        const element = event.target;
+        switch (element.name) {
+            case "name":
+                setName(element.value);
+                break;
+            case "email":
+                setEmail(element.value);
+                break;
+            case "password":
+                setPassword(element.value);
+                break;
+            default:
+                return;
+        }
+    }
+
+    const onSubmit = () => {
+        alert("Data sent to server!");
+    }
+
+    return (
         <main>
-            Register page
+            <input onChange={onInputChange} value={name} name="name" placeholder="Name" />
+            <input onChange={onInputChange} value={email} name="email" placeholder="Email" />
+            <input onChange={onInputChange} value={password} name="password" type="password" placeholder="Password" />
+            <button onClick={onSubmit}>
+                Register
+            </button>
         </main>
     )
 }
